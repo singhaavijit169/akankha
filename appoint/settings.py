c045a18715a8ceca189b22b3df555dc094abb798
avijit <singhaavijit169@gmail.com>
2025-05-08 17:06:56 +0530

    remove whitenoise

--- appoint/settings.py
+++ appoint/settings.py
@@ -71,7 +71,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
